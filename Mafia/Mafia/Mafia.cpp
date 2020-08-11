31db544b7613e0fa4692fbe5adc49fea10ced09f
gunpodong <59949576+gunpodong@users.noreply.github.com>
2020-08-11 20:34:32 +0900

    마을주민
    마을 주민들의 직업을 배정함
    하지만 가끔씩 의사 혹은 마피아가 배정되지 않음

--- Mafia/Mafia/Mafia.cpp
+++ Mafia/Mafia/Mafia.cpp
@@ -3,16 +3,47 @@
 
 using namespace std;
 
-int main()
+int SetJob(int PersonNum)
 {
 	random_device PsnJob;
 
 	mt19937 gen(PsnJob());
 
-	uniform_int_distribution<int> dis(1, 3);
+	uniform_int_distribution<int> dis(2, 3);
 
-	for (int i = 0; i < 5; i++)
+	return dis(gen);
+}
+
+int main()
+{
+	int Town[100] = { 0 };
+	int PersonNum = 0;
+	int Mafia = 0;
+	int Doctor = 0;
+	
+	fill_n(Town, 100, 1);
+
+	cin >> PersonNum;
+
+	for (int i = 0; i < PersonNum; i++)
+	{	
+		if(Mafia < 1 || Doctor < 1)
+		{
+			Town[i] = SetJob(PersonNum);
+			if (Town[i] == 2)
+				Doctor += 1;
+			else if (Town[i] == 3)
+				Mafia += 1;
+
+			if (Doctor > 1)
+				Town[i] = 1;
+			else if (Mafia > 1)
+				Town[i] = 1;
+		}
+	}
+
+	for (int i = 0; i < PersonNum; i++)
 	{
-		cout << dis(gen) << endl;
+		cout << Town[i] << endl;
 	}
 }
