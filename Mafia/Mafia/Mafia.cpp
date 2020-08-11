751c0e68124242c22e5776226eb89308183e79ad
gunpodong <59949576+gunpodong@users.noreply.github.com>
2020-08-11 19:38:51 +0900

    난수생성
    마을 주민들의 직업을 정하기 위한 난수 생성 프로그램

--- Mafia/Mafia/Mafia.cpp
+++ Mafia/Mafia/Mafia.cpp
@@ -1,8 +1,18 @@
 ﻿#include<iostream>
+#include<random>
 
 using namespace std;
 
 int main()
 {
+	random_device PsnJob;
 
+	mt19937 gen(PsnJob());
+
+	uniform_int_distribution<int> dis(1, 3);
+
+	for (int i = 0; i < 5; i++)
+	{
+		cout << dis(gen) << endl;
+	}
 }
