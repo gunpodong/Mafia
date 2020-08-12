f885c95138a4989b3728555b2cd42f42825b787a
gunpodong <59949576+gunpodong@users.noreply.github.com>
2020-08-12 12:17:15 +0900

    아침 오후 밤
    아침,오후,밤을 나누었고, 아무키나 입력하여 다음으로 넘어가게 만들었다.

--- Mafia/Mafia/Mafia.cpp
+++ Mafia/Mafia/Mafia.cpp
@@ -1,5 +1,7 @@
 ﻿#include<iostream>
 #include<random>
+#include<string>
+#include<conio.h>
 
 using namespace std;
 
@@ -11,19 +13,30 @@ int SetJob(int JobNum)
 		return 3;
 }
 
-void Night()
+void Night(int Town[], int PersonNum)
 {
+	_getch();
 	//마피아와 의사의 행동
+	cout << "밤이 되었습니다..." << endl;
+
+	for (int i = 1; i < PersonNum + 1; i++)
+	{
+		cout << Town[i] << endl;
+	}
 }
 
-void Morning()
+void Morning(int Town[],int PersonNum)
 {
+	_getch();
 	//마피아와 의사의 행동결과
+	cout << "날이 밝았습니다..." << endl;
 }
 
-void Afternoon()
+void Afternoon(int Town[], int PersonNum)
 {
+	_getch();
 	//플레이어의 투표와 투표결과
+	cout << "투표시간이 되었습니다..." << endl;
 }
 int main()
 {
@@ -31,10 +44,13 @@ int main()
 	int PersonNum = 0;
 	int Mafia = 0;
 	int Doctor = 0;
+	//char Next[2];
 	
 	fill_n(Town, 100, 1);
 
+	cout << "마을주민의 수를 입력하세요 : ";
 	cin >> PersonNum;
+	cout << endl;
 
 	random_device Psn;
 	mt19937 gen(Psn());
@@ -63,8 +79,16 @@ int main()
 	else
 		cout << "당신의 직업은 의사입니다. 시민을 보호하며 투표를 통해 마피아를 찾아내세요." << endl;
 
-}
-/*for (int i = 1; i < PersonNum + 1; i++)
+	cout << "※다음으로 넘어가기 위해선 아무키나 입력하세요※" << endl << endl;
+
+	while (1)
 	{
-		cout << Town[i] << endl;
-	}*/
+		Night(Town, PersonNum);
+
+		Morning(Town, PersonNum);
+
+		Afternoon(Town, PersonNum);
+	}
+
+	return 0;
+}
